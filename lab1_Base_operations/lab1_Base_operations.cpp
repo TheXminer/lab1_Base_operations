b07a993bad9aed9983f0517db962c619c2860140
Ilya <tehnik4002@gmail.com>
2025-02-24 03:23:21 +0200

    v 0.9

--- lab1_Base_operations/lab1_Base_operations.cpp
+++ lab1_Base_operations/lab1_Base_operations.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <chrono>
 
-void threadFunc(const int* numbers, const int startIndex, const int length, int& sum) {
+void threadFunc(const int* numbers, const long startIndex, const long length, long long& sum) {
     int cursum = 0;
     for (int i = startIndex; i < startIndex + length; i++) {
         cursum += numbers[i] * numbers[i];
@@ -14,24 +14,33 @@ void threadFunc(const int* numbers, const int startIndex, const int length, int&
 int main()
 {
     srand(time(0));
-    int len;
+    long len;
     std::cout << "Enter length of your vector: ";
     std::cin >> len;
+    if (len <= 0) {
+        std::cout << "Error: Vector length must be greater than 0!" << std::endl;
+        return 1;
+    }
     int* vector = new int[len];
 
     for (int i = 0; i < len; i++) {
         vector[i] = rand() % 10;
-        std::cout << vector[i] << std::endl;
+        //std::cout << vector[i] << " ";
     }
 
-    int threadNumber;
-    std::cout << "Enter number of threads: ";
+    long threadNumber;
+    std::cout << "\nEnter number of threads: "; 
     std::cin >> threadNumber;
+    if (threadNumber <= 0 || threadNumber > len) {
+        std::cout << "Error: Number of threads must be between 1 and " << len << "!" << std::endl;
+        delete[] vector;
+        return 1;
+    }
     auto threads = new std::thread[threadNumber];
 
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    int sum = 0;
+    long long sum = 0;
     int num_in_thread = len/ threadNumber;
     int last_thread_num = num_in_thread;
 
@@ -45,7 +54,7 @@ int main()
     }
 
     threads[threadNumber - 1] = 
-        std::thread(threadFunc, vector, (len - 1) * num_in_thread, num_in_thread, std::ref(sum));
+        std::thread(threadFunc, vector, (threadNumber - 1) * num_in_thread, num_in_thread, std::ref(sum));
 
     for (int i = 0; i < threadNumber; i++) {
         threads[i].join();
@@ -54,6 +63,6 @@ int main()
     float vector_norm = sqrt(sum);
 
     auto end_time = std::chrono::high_resolution_clock::now();
-    std::cout << "Norm: " << vector_norm;
-    std::cout << "Time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+    std::cout << "Norm: " << vector_norm << std::endl;
+    std::cout << "Time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count() * 1e-9;
 }
